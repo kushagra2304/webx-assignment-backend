bca60b8029b0a0fb17738c3e5dd62581a57ca745
Kushagra <kushagrasharma826@gmail.com>
2026-02-12 17:42:23 +0530

    second changes

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,  // ✅ port goes inside options
+    port: process.env.DB_PORT || 3306, 
     dialect: "mysql",
   }
 );
